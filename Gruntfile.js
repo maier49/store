8bca0ca701253e78d22dfe58558b3bebf799f5f4
Liang Zhou <lzhoucs@gmail.com>
2016-08-25 16:12:52 -0500

    Add grunt watch task registered as the default task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,22 @@
 module.exports = function (grunt) {
-	require('grunt-dojo2').initConfig(grunt, {
+	const gruntConfig = {
 		dtsGenerator: {
 			options: {
 				main: 'dojo-store-prototype/main'
 			}
+		},
+		watch: {
+			dev: {
+				files: '**/*.ts',
+				tasks: ['dev'],
+				options: {
+					spawn: false
+				}
+			}
 		}
-	});
+	};
+	grunt.initConfig(gruntConfig);
+	require('grunt-dojo2').initConfig(grunt, gruntConfig);
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
 };
